6f1c455a24cd5d071a91a384d93b4ba0a5c01288
chaoyueLin <chaoyue.lin@qq.com>
2022-02-21 19:49:54 +0800

    breakpad

--- ndkCrashDemo/app/src/main/java/com/example/ndkcrashdemo/MainActivity.java
+++ ndkCrashDemo/app/src/main/java/com/example/ndkcrashdemo/MainActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.TextView;
 
 import com.example.ndkcrashdemo.databinding.ActivityMainBinding;
+import com.sample.breakpad.BreakpadInit;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private ActivityMainBinding binding;
+
+    private File externalReportPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         tv.setOnClickListener(v -> {
             tv.setText(stringFromJNI());
         });
+        initBreakPad();
     }
 
     /**
@@ -36,4 +42,19 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+
+    /**
+     * 一般来说，crash捕获初始化都会放到Application中，这里主要是为了大家有机会可以把崩溃文件输出到sdcard中
+     * 做进一步的分析
+     */
+    private void initBreakPad() {
+        if (externalReportPath == null) {
+            externalReportPath = new File(getExternalCacheDir(), "crashDump");
+            if (!externalReportPath.exists()) {
+                externalReportPath.mkdirs();
+            }
+        }
+        BreakpadInit.initBreakpad(externalReportPath.getAbsolutePath());
+    }
 }
